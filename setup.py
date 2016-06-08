1ace5187c0634945129024fa007a99c32a4d1c4a
Ben Kurtovic <ben.kurtovic@gmail.com>
2016-06-08 01:42:27 -0400

    Bump dependencies.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     name = "gitup",
     packages = find_packages(),
     entry_points = {"console_scripts": ["gitup = gitup.script:run"]},
-    install_requires = ["GitPython >= 1.0.1", "colorama >= 0.3.3"],
+    install_requires = ["GitPython >= 2.0.5", "colorama >= 0.3.7"],
     version = __version__,
     author = "Ben Kurtovic",
     author_email = "ben.kurtovic@gmail.com",
