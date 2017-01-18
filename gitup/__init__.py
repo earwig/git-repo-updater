4d1989609a1fa3743e07275170e1c19e8a838c0f
Ben Kurtovic <ben.kurtovic@gmail.com>
2017-01-17 23:43:41 -0600

    release/0.4

--- gitup/__init__.py
+++ gitup/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8  -*-
 #
-# Copyright (C) 2011-2016 Ben Kurtovic <ben.kurtovic@gmail.com>
+# Copyright (C) 2011-2017 Ben Kurtovic <ben.kurtovic@gmail.com>
 # Released under the terms of the MIT License. See LICENSE for details.
 
 """
@@ -8,7 +8,7 @@ gitup: the git repository updater
 """
 
 __author__ = "Ben Kurtovic"
-__copyright__ = "Copyright (C) 2011-2016 Ben Kurtovic"
+__copyright__ = "Copyright (C) 2011-2017 Ben Kurtovic"
 __license__ = "MIT License"
-__version__ = "0.4.dev0"
+__version__ = "0.4"
 __email__ = "ben.kurtovic@gmail.com"
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     name = "gitup",
     packages = find_packages(),
     entry_points = {"console_scripts": ["gitup = gitup.script:run"]},
-    install_requires = ["GitPython >= 2.0.5", "colorama >= 0.3.7"],
+    install_requires = ["GitPython >= 2.1.1", "colorama >= 0.3.7"],
     version = __version__,
     author = "Ben Kurtovic",
     author_email = "ben.kurtovic@gmail.com",
@@ -43,6 +43,7 @@ setup(
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
         "Topic :: Software Development :: Version Control"
     ]
 )
